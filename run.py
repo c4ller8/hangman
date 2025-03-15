302fd8ea688b7ceea26ef687b7dda89802778891
c4ller8 <callumobrien444@gmail.com>
2025-03-15 23:25:05 +0000

    Define function to get a valid word, make a set of used letters.

--- run.py
+++ run.py
@@ -10,4 +10,11 @@ def get_valid_word(words):
     while "-" in word or " " in word:
         word = random.choice(words)
 
-        return word
+        return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in the word
+      alphabet = set(string.ascii_uppercase)
+    used_letters = set() # what the user has guessed
+    
