b656a7032cf8ba84a456664b268b02cd661fc5ac
c4ller8 <callumobrien444@gmail.com>
2025-03-15 21:37:25 +0000

    Add random word list, define a function to draw a valid word to be guessed.

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+from words import words
+
+def get_valid_word(words):
+    word = random.choice(words):    #get random word from list
+    while "-" in word or " " in word:
+        word = random.choice(words)
+
+        return word
