16c1fc3a2c18523e53f4e64000657be98e57bce5
c4ller8 <callumobrien444@gmail.com>
2025-03-16 00:17:12 +0000

    Add user input and err messages.

--- run.py
+++ run.py
@@ -19,3 +19,15 @@ def hangman():
       alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
 
+    # adding user input 
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('Try again. You have already guessed that character')
+
+    else:
+        print('Try again. You have guessed an invalid character.')        
