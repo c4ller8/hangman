fdfa081184cf97988cab746931ec96fa5bbc110c
c4ller8 <callumobrien444@gmail.com>
2025-03-17 14:06:26 +0000

    Print valid word with dashes until letter condition is satisfied.

--- run.py
+++ run.py
@@ -21,11 +21,14 @@ def hangman():
 
     # adding user input 
     while len(word_letters) > 0:
-            # letters used
-            # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-            print('you have used these letters: ', ' '.join(used_letters))
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('you have used these letters: ', ' '.join(used_letters))
 
-            
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
         user_letter = input('Guess a letter: ').upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
