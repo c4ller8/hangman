a393747a755481d81cc302b6118d4f5ffd702503
c4ller8 <callumobrien444@gmail.com>
2025-03-17 14:17:24 +0000

    Add final exit loop conditions.

--- run.py
+++ run.py
@@ -19,11 +19,13 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
 
+    lives = 8
+
     # adding user input 
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         # letters used
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print('you have used these letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
         # what current word is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
@@ -34,12 +36,19 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                lives = lives - 1 # tales away a life if wrong
+                print('Letter is not in word.')
 
         elif user_letter in used_letters:
             print('Try again. You have already guessed that character')
 
         else:
             print('Try again. You have guessed an invalid character.')
-    # gets here when len(word_letters) == 0
+    # gets here when len(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        print('You have died Game Over! THe word was'. word)
+    else:
+        print('You guessed the word', word, '!!')    
 # Call the hangman function to start the game
 hangman()
